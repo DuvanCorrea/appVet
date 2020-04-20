27e13d62e1ac1548f128839ffc8a602fe1177bcc
duvancorrea96 <duvancorrea96@gmail.com>
2020-04-20 00:03:57 -0500

    CRUD sin back listo

--- js/pets.js
+++ js/pets.js
@@ -3,6 +3,8 @@ let btnAgregar = document.getElementById("btn-agregar");
 let inputNombreMascota = document.getElementById("nombre-mascota");
 let inputTipoMascota = document.getElementById("tipo-mascota");
 let inputPropietario = document.getElementById("nombre-propietario");
+let inputIndice = document.getElementById("input-indice");
+let btnAgregarNuevo = document.getElementById("btn-nueva-mascota");
 //console.log(listaTabla);
 
 let mascotas = [
@@ -34,8 +36,9 @@ function listarMascotas() {
                             <td>${mascota.propietario}</td>
                             <td>
                                 <div class="btn-group btn-group-sm" role="group" aria-label="Small button group">
-                                    <button type="button" class="btn btn-success btn-editar">
-                                        <i class="far fa-edit" ></i>
+                                    <button type="button" class="btn btn-success btn-editar" data-indice=${i}
+                                        data-toggle="modal" data-target="#exampleModal">
+                                        <i class="far fa-edit" data-indice=${i}></i>
                                         Editar</button>
                                     <button type="button" class="btn btn-danger btn-eliminar" data-indice=${i}>
                                         <i class="far fa-trash-alt" data-indice=${i}></i>
@@ -51,20 +54,40 @@ function listarMascotas() {
   Array.from(document.getElementsByClassName("btn-eliminar")).forEach((btn) => {
     btn.onclick = eliminarMascota;
   });
+  Array.from(document.getElementsByClassName("btn-editar")).forEach((btn) => {
+    btn.onclick = editarMascota;
+  });
 }
 listarMascotas();
 
 //Agregar mascota
-function agregarMascota() {
-  let mascota = {
-    nombre: inputNombreMascota.value,
-    tipo: inputTipoMascota.value,
-    propietario: inputPropietario.value,
-  };
-  mascotas.push(mascota);
+function enviarMascota() {
+  const accion = btnAgregar.innerHTML;
+
+  switch (accion) {
+    case "Guardar":
+      let mascotaGuardar = {
+        nombre: inputNombreMascota.value,
+        tipo: inputTipoMascota.value,
+        propietario: inputPropietario.value,
+      };
+      console.log(inputIndice.value);
+      mascotas[inputIndice.value] = mascotaGuardar;
+      break;
+
+    case "Crear":
+      let mascotaCrear = {
+        nombre: inputNombreMascota.value,
+        tipo: inputTipoMascota.value,
+        propietario: inputPropietario.value,
+      };
+      mascotas.push(mascotaCrear);
+      break;
+  }
+
   listarMascotas();
 }
-btnAgregar.onclick = agregarMascota;
+btnAgregar.onclick = enviarMascota;
 
 //Eliminar mascota
 function eliminarMascota(e) {
@@ -77,3 +100,39 @@ function eliminarMascota(e) {
   mascotas = nuevasMascotas;
   listarMascotas();
 }
+
+//Editar mascota
+function editarMascota(e) {
+  document.getElementById("btn-agregar").innerHTML = "Guardar";
+  document
+    .getElementById("btn-agregar")
+    .setAttribute("class", "btn btn-success");
+
+  const index = e.target.dataset.indice;
+  const mascota = mascotas[index];
+
+  inputNombreMascota.value = mascota.nombre;
+  inputTipoMascota.value = mascota.tipo;
+  inputPropietario.value = mascota.propietario;
+  inputIndice.value = index;
+
+  document.getElementById(
+    "modal-titulo"
+  ).innerHTML = `Editando a ${mascota.nombre} de ${mascota.propietario}`;
+}
+
+//Reestablcer boton del modal
+function resetModal() {
+  document.getElementById("btn-agregar").innerHTML = "Crear";
+  document
+    .getElementById("btn-agregar")
+    .setAttribute("class", "btn btn-primary");
+
+  inputIndice.value = undefined;
+  inputNombreMascota.value = "";
+  inputPropietario.value = "Propietario";
+  inputTipoMascota.value = "Perro";
+
+  document.getElementById("modal-titulo").innerHTML = `Nueva mascota`;
+}
+btnAgregarNuevo.onclick = resetModal;
